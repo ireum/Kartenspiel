ffe9488836e032c4e1e9d491fd87a86c93a352e4
devdepcomp <teamweb@competec.ch>
2017-08-14 10:08:54 +0200

    Added Unittest with Dataprovider

--- AufgabeOOPK/tests/ColorTest.php
+++ AufgabeOOPK/tests/ColorTest.php
@@ -28,4 +28,47 @@ class ColorTest extends TestCase
         $this->expectExceptionMessage('Invalid color white');
         $this->color = new Color('white');
     }
+
+    /**
+     * @dataProvider validColorProvider
+     */
+    public function testAllValidColors($c)
+    {
+        $this->assertInstanceOf(
+            Color::class,
+            $this->color = new Color($c)
+        );
+    }
+
+    /**
+     * @dataProvider invalidColorProvider
+     */
+    public function testSomeInvalidColors($c)
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Invalid color ' . $c);
+        $this->color = new Color($c);
+    }
+
+    public function validColorProvider()
+    {
+        return array(
+            array('red'),
+            array('green'),
+            array('blue'),
+            array('pink'),
+            array('yellow'),
+            array('orange')
+        );
+    }
+
+    public function invalidColorProvider()
+    {
+        return array(
+            array('brown'),
+            array('black'),
+            array('two'),
+            array(0)
+        );
+    }
 }
